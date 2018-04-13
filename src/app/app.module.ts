fd5d1b00d2717ffbb76a2e891cdaed7a97ae7eaf
Mateus Oliveira <mateus@smnti.com.br>
2018-04-13 17:12:24 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  { path: 'menu', component: MenuComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
